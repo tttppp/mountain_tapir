45f42dbb3b1a7b8f70b9e5878afa44d909161639
tttppp <tttppp@users.noreply.github.com>
2016-04-02 14:01:40 +0100

    Tkinter has been renamed to tkinter in Python 3.

--- mountain_tapir/controller.py
+++ mountain_tapir/controller.py
@@ -16,8 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
-import Tkinter as TK
+try:
+    import tkinter as TK
+except ImportError:
+    import Tkinter as TK
 
 from collections import defaultdict
 from os import path
--- mountain_tapir/ui_vars.py
+++ mountain_tapir/ui_vars.py
@@ -16,7 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import Tkinter as TK
+try:
+    import tkinter as TK
+except ImportError:
+    import Tkinter as TK
 
 from constants import Constants
 
