da123e0dd3807ae59741981aedc353820b32f385
tttppp <tttppp@users.noreply.github.com>
2017-01-19 18:37:19 +0000

    Don't run Coveralls with Python 2.6.
    This works around the InsecurePlatformWarning by not sending Coveralls the
    coverage information for Python 2.6.

--- run_coveralls.py
+++ run_coveralls.py
@@ -3,11 +3,11 @@
 # Taken from:
 # http://stackoverflow.com/questions/32757765/conditional-commands-in-tox-tox-travis-ci-and-coveralls
 
-import os
+import os, sys
 
 from subprocess import call
 
 if __name__ == '__main__':
-    if 'TRAVIS' in os.environ:
+    if 'TRAVIS' in os.environ and sys.version_info[:2] != (2, 6):
         rc = call('coveralls')
         raise SystemExit(rc)
