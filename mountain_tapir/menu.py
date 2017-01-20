d2e3bd9b64ad78670a1ac853db75729f2f39a00a
tttppp <tttppp@users.noreply.github.com>
2017-01-20 22:00:27 +0000

    Convert the menu to two rows of controls.
    The rotate button had shifted the save button off the edge of the screen
    with the default window size.

--- mountain_tapir/menu.py
+++ mountain_tapir/menu.py
@@ -28,54 +28,61 @@ class Menu:
     def __init__(self, parent, controller):
         self.myParent = parent
         
-        self.menuFrame = TK.Frame(self.myParent)
-        self.menuFrame.pack(side=TK.BOTTOM)
+        # The first row of controls.
+        self.menuFrameA = TK.Frame(self.myParent)
         
-        self.loadToolButton = TK.Button(self.menuFrame, text='Load', command=lambda : controller.selectTool(Tool.LOAD))
+        self.loadToolButton = TK.Button(self.menuFrameA, text='Load', command=lambda : controller.selectTool(Tool.LOAD))
         self.loadToolButton.pack(side=TK.LEFT)
         
-        self.swapToolButton = TK.Button(self.menuFrame, text='Swap', command=lambda : controller.selectTool(Tool.SWAP))
+        self.swapToolButton = TK.Button(self.menuFrameA, text='Swap', command=lambda : controller.selectTool(Tool.SWAP))
         self.swapToolButton.pack(side=TK.LEFT)
         
-        self.emptyToolButton = TK.Button(self.menuFrame, text='Empty', command=lambda : controller.selectTool(Tool.EMPTY))
+        self.emptyToolButton = TK.Button(self.menuFrameA, text='Empty', command=lambda : controller.selectTool(Tool.EMPTY))
         self.emptyToolButton.pack(side=TK.LEFT)
         
-        self.rotateToolButton = TK.Button(self.menuFrame, text='Rotate', command=lambda : controller.selectTool(Tool.ROTATE))
+        self.rotateToolButton = TK.Button(self.menuFrameA, text='Rotate', command=lambda : controller.selectTool(Tool.ROTATE))
         self.rotateToolButton.pack(side=TK.LEFT)
         
-        self.shuffleButton = TK.Button(self.menuFrame, text='Shuffle', command=controller.shuffle)
+        self.shuffleButton = TK.Button(self.menuFrameA, text='Shuffle', command=controller.shuffle)
         self.shuffleButton.pack(side=TK.LEFT)
         
-        self.collageAlgorithmButton = TK.Button(self.menuFrame, text='Collage', command=lambda : controller.setAlgorithm(Algorithm.COLLAGE))
+        self.collageAlgorithmButton = TK.Button(self.menuFrameA, text='Collage', command=lambda : controller.setAlgorithm(Algorithm.COLLAGE))
         self.collageAlgorithmButton.pack(side=TK.LEFT)
-        self.gridAlgorithmButton = TK.Button(self.menuFrame, text='Grid', command=lambda : controller.setAlgorithm(Algorithm.GRID))
+        self.gridAlgorithmButton = TK.Button(self.menuFrameA, text='Grid', command=lambda : controller.setAlgorithm(Algorithm.GRID))
         self.gridAlgorithmButton.pack(side=TK.LEFT)
-        self.frameAlgorithmButton = TK.Button(self.menuFrame, text='Frame', command=lambda : controller.setAlgorithm(Algorithm.FRAME))
+        self.frameAlgorithmButton = TK.Button(self.menuFrameA, text='Frame', command=lambda : controller.setAlgorithm(Algorithm.FRAME))
         self.frameAlgorithmButton.pack(side=TK.LEFT)
         
-        self.widthLabel = TK.Label(self.menuFrame, text='Width:')
+        # The second row of controls.
+        self.menuFrameB = TK.Frame(self.myParent)
+        
+        self.widthLabel = TK.Label(self.menuFrameB, text='Width:')
         self.widthLabel.pack(side=TK.LEFT)
-        self.widthEntry = TK.Entry(self.menuFrame, textvariable=controller.uiVars.widthVar)
+        self.widthEntry = TK.Entry(self.menuFrameB, textvariable=controller.uiVars.widthVar)
         self.widthEntry.bind('<FocusOut>', controller.updateWidth)
         self.widthEntry.bind('<Return>', controller.updateWidth)
         self.widthEntry.bind('<KP_Enter>', controller.updateWidth)
         self.widthEntry.pack(side=TK.LEFT)
-        self.heightLabel = TK.Label(self.menuFrame, text='Height:')
+        self.heightLabel = TK.Label(self.menuFrameB, text='Height:')
         self.heightLabel.pack(side=TK.LEFT)
-        self.heightEntry = TK.Entry(self.menuFrame, textvariable=controller.uiVars.heightVar)
+        self.heightEntry = TK.Entry(self.menuFrameB, textvariable=controller.uiVars.heightVar)
         self.heightEntry.bind('<FocusOut>', controller.updateHeight)
         self.heightEntry.bind('<Return>', controller.updateHeight)
         self.heightEntry.bind('<KP_Enter>', controller.updateHeight)
         self.heightEntry.pack(side=TK.LEFT)
         
-        self.minusRegionButton = TK.Button(self.menuFrame, text='-', command=lambda : controller.addRegions(-1))
+        self.minusRegionButton = TK.Button(self.menuFrameB, text='-', command=lambda : controller.addRegions(-1))
         self.minusRegionButton.pack(side=TK.LEFT)
-        self.regionsDisplay = TK.Label(self.menuFrame, textvariable=controller.uiVars.regionsVar)
+        self.regionsDisplay = TK.Label(self.menuFrameB, textvariable=controller.uiVars.regionsVar)
         self.regionsDisplay.pack(side=TK.LEFT)
-        self.regionsLabel = TK.Label(self.menuFrame, text='regions')
+        self.regionsLabel = TK.Label(self.menuFrameB, text='regions')
         self.regionsLabel.pack(side=TK.LEFT)
-        self.plusRegionButton = TK.Button(self.menuFrame, text='+', command=lambda : controller.addRegions(1))
+        self.plusRegionButton = TK.Button(self.menuFrameB, text='+', command=lambda : controller.addRegions(1))
         self.plusRegionButton.pack(side=TK.LEFT)
         
-        self.saveButton = TK.Button(self.menuFrame, text='Save', command=controller.save)
+        self.saveButton = TK.Button(self.menuFrameB, text='Save', command=controller.save)
         self.saveButton.pack(side=TK.RIGHT)
+
+        # Pack the two rows of controls.        
+        self.menuFrameB.pack(side=TK.BOTTOM)
+        self.menuFrameA.pack(side=TK.BOTTOM)
