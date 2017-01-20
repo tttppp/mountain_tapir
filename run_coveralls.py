e48ac1de96b4763bf3bae4b15bdc0de820178fcd
tttppp <tttppp@users.noreply.github.com>
2017-01-20 18:26:09 +0000

    Don't send pypy information to Coveralls.

--- run_coveralls.py
+++ run_coveralls.py
@@ -3,11 +3,13 @@
 # Taken from:
 # http://stackoverflow.com/questions/32757765/conditional-commands-in-tox-tox-travis-ci-and-coveralls
 
-import os, sys
+import os, sys, platform
 
 from subprocess import call
 
 if __name__ == '__main__':
-    if 'TRAVIS' in os.environ and sys.version_info[:2] != (2, 6):
+    if 'TRAVIS' in os.environ \
+       and sys.version_info[:2] != (2, 6) \
+       and platform.python_implementation() != 'PyPy':
         rc = call('coveralls')
         raise SystemExit(rc)
