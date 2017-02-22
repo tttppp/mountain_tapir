22b8cacf341677aac4081f407f48520bb25cf8b8
tttppp <tttppp@users.noreply.github.com>
2017-02-22 19:23:12 +0000

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ test_requirements = [
 
 setup(
     name='mountain_tapir',
-    version='1.0.2',
+    version='1.0.3',
     description="Mountain Tapir Collage Maker is a tool for combining images into collages.",
     long_description=readme + '\n\n' + history,
     author="tttppp",
