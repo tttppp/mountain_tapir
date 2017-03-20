a687005a6e8033d5f6c2bc964eb611fac012da59
tttppp <tttppp@users.noreply.github.com>
2017-03-20 17:58:26 +0000

    Fix snap to use python 3.

--- wrappers/mountain_tapir.sh
+++ wrappers/mountain_tapir.sh
@@ -2,5 +2,5 @@
 export TCL_LIBRARY=$SNAP/usr/share/tcltk/tcl8.6:$TCL_LIBRARY:$TK_LIBRARY
 export TK_LIBRARY=$SNAP/usr/share/tcltk/tk8.6:$TK_LIBRARY:$TCL_LIBRARY
 
-python -m mountain_tapir.mountain_tapir
+$SNAP/usr/bin/python3 -m mountain_tapir.mountain_tapir
 
