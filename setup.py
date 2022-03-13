b243ea88e41f684d4e2050c2ec618805f9af1529
tttppp <tttppp@users.noreply.github.com>
2022-03-13 14:06:45 +0000

    Try to upgrade to Python 3.10.

--- setup.py
+++ setup.py
@@ -61,12 +61,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
     tests_require=test_requirements
